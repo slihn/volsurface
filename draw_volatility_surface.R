cf5d53de976864815cecf7878f63ac40c3433bf4
slihn <slihn@novus.com>
2014-11-07 14:30:06 -0500

    Surface plot by R, data is a bit noisy

---
+++ draw_volatility_surface.R
@@ -0,0 +1,53 @@
+library("plot3D")
+library("plyr")
+
+setwd("~/Windows/git/volsurface")
+
+spx_data0 <- read.csv("data/SPX_options_2013_1231.csv")
+spx_data1 <- transform(spx_data0, expiration_date = as.Date(expiration_date, format="%m/%d/%Y"))
+spx_data1$exp_days <- as.numeric(spx_data1[["expiration_date"]] - start_date)
+
+spx_data <- spx_data1
+
+
+start_close <- 1848.36
+start_date <- as.Date("12/31/2013", format="%m/%d/%Y")
+
+spx_dates <- unique(spx_data[["expiration_date"]])
+spx_strikes <- unique(spx_data[["strike"]])
+spx_days <- unique(spx_data[["exp_days"]])
+spx_fwd_strikes <- log(spx_strikes / start_close)
+
+imp_vol_data <- function(in_days, in_strike) {
+  d <- subset(spx_data, exp_days == in_days & strike == in_strike & substring(root_symbol,1,3) == "SPX")
+  return(d)
+}
+imp_vol <- function(in_days, in_strike) {
+  d <- imp_vol_data(in_days, in_strike)
+  return(mean(d[["implied_volatility_1545"]], na.rm=TRUE))
+}
+imp_vol_mat <- function(days, strike) {
+  m2 <- days
+  for (r in seq(nrow(m2)))
+    for (c in seq(ncol(m2)))
+      m2[[r, c]] <- imp_vol(days[r,c], strike[r,c])
+  return(m2)
+}
+
+x <- spx_days[spx_days <= 100]
+y <- spx_strikes[abs(spx_strikes-start_close) <= 200]
+M <- mesh(x, y)
+
+x3d <- M$x
+y3d <- M$y
+vol3d <- imp_vol_mat(x3d, y3d)
+
+surf3D(x = x3d, y = y3d, z = vol3d,
+       colkey=FALSE, contour=TRUE, border = "black", 
+       facets=FALSE, # turn this on and off
+       bty="b2", theta=-45, phi=45,
+       main="Volatility Surface", xlab="Days", ylab="Strike", zlab="Imp Vol")
+
+
+
+
